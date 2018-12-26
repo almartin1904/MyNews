08f706d5846fb94013e2ab82f805960772f9489f
alice <alice.martinez.28@gmail.com>
2018-12-26 12:48:49 +0100

    modify AlreadyReadArticlesTest

---
+++ app/src/androidTest/java/com/openclassroom/alice/mynews/Model/AlreadyReadArticleTest.java
@@ -0,0 +1,28 @@
+package com.openclassroom.alice.mynews.Model;
+
+import com.openclassroom.alice.mynews.Model.AlreadyReadArticles;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by Alice on 26 December 2018.
+ */
+public class AlreadyReadArticleTest {
+
+    private static final String TITLE_TEST = "Test";
+
+    @Test
+    public void hasAlreadyBeenReadTest(){
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        AlreadyReadArticles alreadyReadArticle=AlreadyReadArticles.getInstance(appContext);
+        alreadyReadArticle.addNewTitleToList(TITLE_TEST);
+        assertTrue(alreadyReadArticle.hasAlreadyBeenRead(TITLE_TEST));
+    }
+}
